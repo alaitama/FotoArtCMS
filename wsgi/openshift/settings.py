7293ad890475cb9f2c020f486b2f25007a71ad7f
Isaac Bythewood <isaac@bythewood.me>
2012-07-27 06:02:51 +0000

    Remove htaccess update settings for media.

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -181,7 +181,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, '..', 'static')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/media/'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
