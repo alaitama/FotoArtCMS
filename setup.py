2131cefe887cf4ebc3ef8a6b715b583ffb8b6cc9
Romain Hardouin <romain_hardouin@yahoo.fr>
2013-05-15 22:47:21 +0200

    Bump version and update requirements version

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='mezzanine-openshift',
-    version='1.0',
+    version='1.1',
     description='Mezzanine configured for deployment on OpenShift.',
     author='Isaac Bythewood',
     author_email='isaac@bythewood.me',
     url='http://isaacbythewood.com/',
     install_requires=[
-        'Django==1.4.2',
-        'mezzanine==1.2.4',
-        'django_compressor==1.2'
+        'Django==1.5.1',
+        'mezzanine==1.4.6',
+        'django_compressor==1.3'
     ],
 )
