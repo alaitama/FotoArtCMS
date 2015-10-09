594b0324ba410045d27c3feb75738e87c8b710e7
ramsys <ramborghacklab@gmail.com>
2015-10-09 23:06:21 +0200

    modified:   wsgi/openshift/settings/production.py
    Add PostgreSQL config

--- wsgi/openshift/settings/production.py
+++ wsgi/openshift/settings/production.py
@@ -4,14 +4,51 @@ from settings import *
 DEBUG = True
 ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
 
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'sqlite3.db'),
     }
 }
+"""
+# a setting to determine whether we are running on OpenShift
+ON_OPENSHIFT = False
+EXISTS_POSTGRESQL = False
+if os.environ.has_key('OPENSHIFT_REPO_DIR'):
+    ON_OPENSHIFT = True
+if os.environ.has_key('OPENSHIFT_APP_NAME'):
+    DB_NAME = os.environ['OPENSHIFT_APP_NAME']
+if os.environ.has_key('OPENSHIFT_POSTGRESQL_DB_USERNAME'):
+    EXISTS_POSTGRESQL = True
+    DB_USER = os.environ['OPENSHIFT_POSTGRESQL_DB_USERNAME']
+if os.environ.has_key('OPENSHIFT_POSTGRESQL_DB_PASSWORD'):
+    DB_PASSWD = os.environ['OPENSHIFT_POSTGRESQL_DB_PASSWORD']
+if os.environ.has_key('OPENSHIFT_POSTGRESQL_DB_HOST'):
+    DB_HOST = os.environ['OPENSHIFT_POSTGRESQL_DB_HOST']
+if os.environ.has_key('OPENSHIFT_POSTGRESQL_DB_PORT'):
+    DB_PORT = os.environ['OPENSHIFT_POSTGRESQL_DB_PORT']
 
+if EXISTS_POSTGRESQL:
+    # os.environ['OPENSHIFT_DB_*'] variables can be used with databases created
+    # with rhc app cartridge add (see /README in this git repo)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': DB_NAME,               # Or path to database file if using sqlite3.
+            'USER': DB_USER,               # Not used with sqlite3.
+            'PASSWORD': DB_PASSWD,         # Not used with sqlite3.
+            'HOST': DB_HOST,               # Set to empty string for localhost. Not used with sqlite3.
+            'PORT': DB_PORT,               # Set to empty string for default. Not used with sqlite3.
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'sqlite3.db'),
+        }
+    }
 
 SECRET_KEY = 'your-super-secret-key'
 
@@ -22,4 +59,3 @@ MEDIA_ROOT = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR'), 'media')
 
 from mezzanine.utils.conf import set_dynamic_settings
 set_dynamic_settings(globals())
-
